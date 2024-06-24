786640713777a596856576f3e76bfadfa861c93d
秋水 <1123993881@qq.com>
2024-06-24 10:37:48 +0800

    fix: rhino 8 patch.

---
+++ WatermarkPainter/Data.cs
@@ -0,0 +1,80 @@
+﻿using Grasshopper;
+using SimpleGrasshopper.Attributes;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenFileDialog = Rhino.UI.OpenFileDialog;
+using System.IO;
+using System;
+
+namespace WatermarkPainter;
+internal static partial class Data
+{
+    [Range(0, 5, 3)]
+    [Setting, Config("Image Ratio")]
+    private static readonly float _ImageRatio = 1f;
+
+    [Setting]
+    private static readonly string _ImagePath = string.Empty;
+
+    [Setting, Config("Alignment")]
+    private static readonly ContentAlignment _Alignment = ContentAlignment.BottomRight;
+
+    [Range(5, 60)]
+    [Setting, Config("FPS")]
+    private static readonly int _GifFps = 15;
+
+    [Config("File Location")]
+    private static bool ChangeFile
+    {
+        get => false;
+        set
+        {
+            var open = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif",
+                Title = "A Image File",
+                FileName = ImagePath,
+                MultiSelect = false
+            };
+            if (open.ShowOpenDialog())
+            {
+                ImagePath = open.FileName;
+            };
+        }
+    }
+    static readonly DateTime startTime = DateTime.Now;
+    static int _frameCount;
+    static Image _readImage;
+    public static Image DrawImage
+    {
+        get
+        {
+            if (HasFrameCount)
+            {
+                var i = (int)((DateTime.Now - startTime).TotalSeconds * GifFps) % _frameCount;
+                _readImage.SelectActiveFrame(FrameDimension.Time, i);
+            }
+            return _readImage;
+        }
+    }
+
+    public static bool HasFrameCount => _frameCount > 1;
+
+    static Data()
+    {
+        OnPropertyChanged += (s, e) =>
+        {
+            Instances.ActiveCanvas.ScheduleRegen(10);
+        };
+
+        OnImagePathChanged += LoadImage;
+    }
+
+    private static void LoadImage(string path)
+    {
+        if (!File.Exists(path)) return;
+
+        _readImage = Image.FromFile(path);
+        _frameCount = _readImage.GetFrameCount(new FrameDimension(_readImage.FrameDimensionsList[0]));
+    }
+}
--- WatermarkPainter/WatermarkPainterInfo.cs
+++ WatermarkPainter/WatermarkPainterInfo.cs
@@ -1,70 +1,39 @@
-﻿using Eto.Forms;
-using Grasshopper;
-using Grasshopper.GUI.Canvas;
-using Grasshopper.GUI;
+﻿using Grasshopper.GUI;
 using Grasshopper.Kernel;
+using HarmonyLib;
+using SimpleGrasshopper.Util;
 using System;
 using System.Drawing;
 
-namespace WatermarkPainter
+namespace WatermarkPainter;
+
+public class WatermarkPainterInfo : GH_AssemblyInfo
 {
-    public class WatermarkPainterInfo : GH_AssemblyInfo
+    public override string Name => "WatermarkPainter";
+
+    //Return a 24x24 pixel bitmap to represent this GHA library.
+    public override Bitmap Icon => typeof(WatermarkPainterInfo).Assembly.GetBitmap("WatermarkIcon128.png");
+
+    //Return a short string describing the purpose of this GHA library.
+    public override string Description => "Add the watermark to your gh canvas.";
+
+    public override Guid Id => new ("f4a5f9af-a365-4ba0-86cb-cf7bdf4d2dd6");
+
+    //Return a string identifying you or your company.
+    public override string AuthorName => "秋水";
+
+    //Return a string representing your preferred contact details.
+    public override string AuthorContact => "1123993881@qq.com";
+}
+
+partial class SimpleAssemblyPriority
+{
+    protected override void DoWithEditor(GH_DocumentEditor editor)
     {
-        public override string Name => "WatermarkPainter";
+        var harmony = new Harmony("grasshopper.WatermarkPainter.patch");
+        harmony.PatchAll();
 
-        //Return a 24x24 pixel bitmap to represent this GHA library.
-        public override Bitmap Icon => null;
-
-        //Return a short string describing the purpose of this GHA library.
-        public override string Description => "Add the watermark to your gh canvas.";
-
-        public override Guid Id => new Guid("f4a5f9af-a365-4ba0-86cb-cf7bdf4d2dd6");
-
-        //Return a string identifying you or your company.
-        public override string AuthorName => "秋水";
-
-        //Return a string representing your preferred contact details.
-        public override string AuthorContact => "1123993881@qq.com";
-    }
-
-    public class WatermarkPainterAssemblyPriority : GH_AssemblyPriority
-    {
-        public override GH_LoadingInstruction PriorityLoad()
-        {
-            Instances.CanvasCreated += Instances_CanvasCreated;
-            return GH_LoadingInstruction.Proceed;
-        }
-
-        void Instances_CanvasCreated(GH_Canvas canvas)
-        {
-            Instances.CanvasCreated -= Instances_CanvasCreated;
-
-            GH_DocumentEditor editor = Instances.DocumentEditor;
-            if (editor == null)
-            {
-                Instances.ActiveCanvas.DocumentChanged += ActiveCanvas_DocumentChanged;
-                return;
-            }
-            DoingSomethingFirst(editor);
-        }
-
-        void ActiveCanvas_DocumentChanged(GH_Canvas sender, GH_CanvasDocumentChangedEventArgs e)
-        {
-            Instances.ActiveCanvas.DocumentChanged -= ActiveCanvas_DocumentChanged;
-
-            GH_DocumentEditor editor = Instances.DocumentEditor;
-            if (editor == null)
-            {
-                MessageBox.Show("Watermark Painter can't find the menu!");
-                return;
-            }
-            DoingSomethingFirst(editor);
-        }
-
-        void DoingSomethingFirst(GH_DocumentEditor editor)
-        {
-            WPainter.Init();
-            MenuCreater.CreateMenu(editor);
-        }
+        Data.ImagePath = Data.ImagePath;
+        base.DoWithEditor(editor);
     }
 }
